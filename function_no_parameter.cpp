967fcfbcdbbe85578eb5278ee205b1a0c315d8fd
shujaatfida512-cyber <shujaatfida512@gmail.com>
2026-03-10 10:39:03 +0700

    Implement rectangle area calculation program

---
+++ function_no_parameter.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
+
+int luasPersegi()
+{
+    return panjang * lebar;
+}
+
+void output()
+{
+    cout << "Hasilnya : " << luasPersegi();
+}
+
+int main()
+{
+    input();
+    output();
+}
